1508799e76e1e8dfb1ae258a51d88dd0b3f16888
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-29 00:43:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ async function starts() {
     			const apakah = ['Si','No']
                         const kapankah = ['Otro día','Otra semana','Otro mes','Otro año']
 			const botNumber = client.user.jid
-			const ownerNumber = ["524591347471@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["593997889284@s.whatsapp.net"] // replace this with your number
 			const nomorOwner = [ownerNumber]
 	                const isGroup = from.endsWith('@g.us')
 			const totalchat = await client.chats.all()
